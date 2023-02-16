dc45c358fb24fffe8b597d98af0641f6f3665a52
suhaibkhan699 <khansuhaib699@gmail.com>
2023-02-16 15:49:24 +0000

    Q2

--- src/main/java/client/GrpcClient.java
+++ src/main/java/client/GrpcClient.java
@@ -20,10 +20,17 @@ public class GrpcClient {
         { 9, 10, 11, 12 },
         { 13, 14, 15, 16 } };
 
-    int B[][] = { { 1, 2, 3, 4 },
-        { 5, 6, 7, 8 },
-        { 9, 10, 11, 12 },
-        { 13, 14, 15, 16 } };
+    /*
+     * int B[][] = { { 1, 2, 3, 4 },
+     * { 5, 6, 7, 8 },
+     * { 9, 10, 11, 12 },
+     * { 13, 14, 15, 16 } };
+     */
+
+    int B[][] = { { 2, 3, 4, 5 },
+        { 6, 7, 8, 9 },
+        { 10, 11, 12, 13 },
+        { 14, 15, 16, 17 } };
     MatrixReply A3M1 = stub.multiplyBlock(MatrixRequest.newBuilder()// First Result Block Calculation
         .setA00(A[0][0])
         .setA01(A[0][1])
